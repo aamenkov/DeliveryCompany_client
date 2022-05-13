50b99800c9b20c5a0c0086776f112fe116424ccc
aamenkov <aamenkov@mail.ru>
2022-05-13 04:52:15 +0300

    add models

---
+++ DeliveryCompany_WPF/Models/Application.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DeliveryCompany_Wpf.Models
+{
+    public class Application
+    {
+        public Guid ApplicationId { get; set; }
+        public string ReceivingAddress { get; set; }
+        public string ReceivingTown { get; set; }
+        public string DeliveryAddress { get; set; }
+        public string DeliveryTown { get; set; }
+        public int Weight { get; set; }
+        public int Length { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public double Volume
+        {
+            get { return (double)Length * Width * Height * 0.000001; }
+            private set { ; }
+        }
+
+        public DateTime ReceivingDateTime;
+        public string PhoneNumber { get; set; }
+        public string Status { get; set; }
+        public string Message { get; set; }
+        public int DepartmentId { get; set; }
+    }
+}
---
+++ DeliveryCompany_WPF/Models/Department.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Transactions;
+
+namespace DeliveryCompany_Wpf.Models
+{
+    public class Department
+    {
+        public int DepartmentId { get; set; }
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Коллекция заявок отделения
+        /// </summary>
+        public List<Application> ApplicationList { get; set; }
+    }
+}
